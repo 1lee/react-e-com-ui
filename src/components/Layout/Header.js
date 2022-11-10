03c50830f622d2372754edfc0dfb184ca5632b57
1lee <1lee.burrell@gmail.com>
2022-11-10 13:28:19 -0500

    open/close modal

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = props => {
 		<Fragment>
 			<header className={classes.header}>
 				<h1>Moon Magic</h1>
-				<HeaderCartButton />
+				<HeaderCartButton onClick={props.onShowCart} />
 			</header>
 			<div className={classes['main-image']}>
 				<img src={moonPic} alt='a blood moon' />
