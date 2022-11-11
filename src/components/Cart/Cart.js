ec8e6ecdf2b5babe9f419d6ed898ae9d033b776b
1lee <1lee.burrell@gmail.com>
2022-11-11 15:51:26 -0500

    added CartItem.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,6 +4,8 @@ import CartContext from '../store/cart-context';
 import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
+import CartItem from './CartItem';
+
 const Cart = props => {
 	const cartCtx = useContext(CartContext);
 
@@ -13,10 +15,21 @@ const Cart = props => {
 
 	const hasItems = cartCtx.items.length > 0;
 
+	const cartItemRemoveHandler = id => {};
+
+	const cartItemAddHandler = item => { };
+	
+
 	const cartItems = (
 		<ul className={classes['cart-items']}>
 			{cartCtx.items.map(item => (
-				<li>{item.name}</li>
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}>
+					{item.name}
+				</CartItem>
 			))}
 		</ul>
 	);
